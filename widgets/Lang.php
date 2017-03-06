afffe7d637b3e8e176fb5e853506723c28c8b8d2
Alexey Dench <dench@i.ua>
2017-03-06 22:33:05 +0200

    update

--- widgets/Lang.php
+++ widgets/Lang.php
@@ -8,16 +8,85 @@
 
 namespace dench\language\widgets;
 
+use Yii;
 use dench\language\models\Language;
-use yii\base\Widget;
+use yii\bootstrap\Widget;
+use yii\base\InvalidConfigException;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 class Lang extends Widget
 {
+    public $short = false;
+
+    public $current;
+
+    public $langs = [];
+
+    /**
+     * Initializes the widget.
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (!isset($this->current)) {
+            $this->current = Language::getCurrent();
+            //throw new InvalidConfigException("The 'current' option is required.");
+        }
+
+        if (!isset($this->langs)) {
+            $this->langs = Language::nameList();
+            //throw new InvalidConfigException("The 'langs' option is required.");
+        }
+    }
+
+    /**
+     * Renders the widget.
+     */
     public function run()
     {
-        return $this->render('lang', [
-            'current' => Language::getCurrent(),
-            'langs' => Language::nameList(),
-        ]);
+        return $this->renderItems();
+    }
+
+    /**
+     * Renders widget items.
+     */
+    public function renderItems()
+    {
+        $items = [];
+        foreach ($this->langs as $key => $name) {
+            $items[] = $this->renderItem($name, $key);
+        }
+
+        Html::addCssClass($this->options, 'navbar-nav');
+
+        return Html::tag('ul', implode("\n", $items), $this->options);
+    }
+
+    /**
+     * Renders a widget's item.
+     * @param string|array $item the item to render.
+     * @return string the rendering result.
+     * @throws InvalidConfigException
+     */
+    public function renderItem($name, $key)
+    {
+        $options = [];
+        $linkOptions = [];
+        $url = Url::to(['', 'lang' => $key]);
+
+        if (Yii::$app->language == $key) {
+            Html::addCssClass($options, 'active');
+        }
+
+        if ($this->short) {
+            $name = mb_substr($name, 0, 3, 'UTF-8');
+        }
+
+        Html::addCssClass($options, 'nav-item');
+        Html::addCssClass($linkOptions, 'nav-link');
+
+        return Html::tag('li', Html::a($name, $url, $linkOptions), $options);
     }
 }
